f0bea8866b379e3dac86d42d6a970c30dbff99f2
playgit2001 <2719296038@qq.com>
2022-08-12 15:15:26 +0800

    动态规划，算雨水

--- src/main/java/Test32.java
+++ src/main/java/Test32.java
@@ -1,3 +1,23 @@
-public class Test32 {
+import java.util.Deque;
+import java.util.LinkedList;
 
+public class Test32 {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
 }
--- src/main/java/Test42.java
+++ src/main/java/Test42.java
@@ -1,5 +1,22 @@
 public class Test42 {
+    //leetcode 42接雨水
+    //动态规划方法的体会，使用场景。在这个题目中，这个能接的雨水值，跟左右两端的最大高度有关，最大高度之和减去当前高度，就是可接雨水值。
+    //而动态规划的使用，是因为在测试计算最大高度时，从左往右，每个柱的左最大高度与上一个最大高度有关。
+    //从右往左，算右最大高度也是如此。
     public int trap(int[] height) {
-
+        int length=height.length;
+        int sum=0;
+        if(length<2)return 0;
+        int []dpl=new int[length];
+        int []dpr=new int[length];
+        dpl[0]=height[0];
+        dpr[length-1]=height[length-1];
+        for(int i=1;i<length;i++)
+            dpl[i]=Math.max(dpl[i-1],height[i]);
+        for(int j=length-2;j>=0;j--)
+            dpr[j]=Math.max(dpr[j+1],height[j]);
+        for(int i=0;i<length;i++)
+            sum+=Math.min(dpl[i],dpr[i])-height[i];
+        return sum;
     }
 }
